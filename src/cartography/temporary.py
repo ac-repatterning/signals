93b04db12449130461579402df7ccc82f39387ad
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-03-05 23:31:46 +0000

    Add src/cartography/temporary.py

---
+++ src/cartography/temporary.py
@@ -0,0 +1,43 @@
+"""Module temporary.py"""
+import datetime
+import uuid
+
+import geopandas
+import pytz
+
+import config
+
+
+class Temporary:
+    """
+    Retrieves a temporary data set for testing
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.__minutes = 35
+
+        # Instances
+        self.__configurations = config.Config()
+
+    def __call__(self) -> geopandas.GeoDataFrame:
+        """
+
+        :return:
+        """
+
+        baseline = datetime.datetime.now(pytz.utc)
+
+        try:
+            frame = geopandas.read_file(
+                filename=self.__configurations.area_)
+            frame['warningId'] = str(uuid.uuid4())
+            frame['validFromDate'] = baseline + datetime.timedelta(minutes=self.__minutes)
+            frame['validToDate'] = baseline + datetime.timedelta(minutes=3*self.__minutes)
+            frame.info()
+            return frame
+        except FileNotFoundError as err:
+            raise err from err
